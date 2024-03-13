b977cefcfd6bc48da79cc8e217de2a2d120fbca7
lethanhson1234 <tinhnghesohu@gmail.com>
2024-03-13 09:39:45 +0700

    20240313

--- Business/Implement/NhanVienTaiKhoanBusiness.cs
+++ Business/Implement/NhanVienTaiKhoanBusiness.cs
@@ -1,12 +1,48 @@
 ﻿
+using Data.Repository.Implement;
+
 namespace Business.Implement
 {
-    public class NhanVienTaiKhoanBusiness : BaseBusiness<NhanVienTaiKhoan, INhanVienTaiKhoanRepository>, INhanVienTaiKhoanBusiness
-    {
-        private readonly INhanVienTaiKhoanRepository _NhanVienTaiKhoanRepository;
-        public NhanVienTaiKhoanBusiness(INhanVienTaiKhoanRepository NhanVienTaiKhoanRepository) : base(NhanVienTaiKhoanRepository)
-        {
-            _NhanVienTaiKhoanRepository = NhanVienTaiKhoanRepository;
-        }
-    }
+	public class NhanVienTaiKhoanBusiness : BaseBusiness<NhanVienTaiKhoan, INhanVienTaiKhoanRepository>, INhanVienTaiKhoanBusiness
+	{
+		private readonly INhanVienTaiKhoanRepository _NhanVienTaiKhoanRepository;
+		public NhanVienTaiKhoanBusiness(INhanVienTaiKhoanRepository NhanVienTaiKhoanRepository) : base(NhanVienTaiKhoanRepository)
+		{
+			_NhanVienTaiKhoanRepository = NhanVienTaiKhoanRepository;
+		}
+		public override async Task<NhanVienTaiKhoan> SaveAsync(NhanVienTaiKhoan model)
+		{
+			Initialization(model);
+			if (!string.IsNullOrEmpty(model.Code))
+			{
+				NhanVienTaiKhoan modelExist = new NhanVienTaiKhoan();
+				if (model.ParentID > 1)
+				{
+					modelExist = await _NhanVienTaiKhoanRepository.GetByCodeAsync(model.Code);					
+				}
+				if (model.ID > 0)
+				{
+					if (modelExist.ID == 0)
+					{
+						await _NhanVienTaiKhoanRepository.UpdateAsync(model);
+					}
+					else
+					{
+						if (modelExist.ID == model.ID)
+						{
+							await _NhanVienTaiKhoanRepository.UpdateAsync(model);
+						}
+					}
+				}
+				else
+				{
+					if (modelExist.ID == 0)
+					{
+						await _NhanVienTaiKhoanRepository.AddAsync(model);
+					}
+				}
+			}
+			return model;
+		}
+	}
 }
